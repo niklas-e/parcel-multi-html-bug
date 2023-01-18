3cb4d4dae92ccb2328eea9259235858c3f28ef17
Niklas Engblom <niklas-e@users.noreply.github.com>
2023-01-18 16:34:59 +0200

    Partial repro

---
+++ src/c.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("app");
+if (!rootElement) throw new Error("Couldn't get root for react");
+const root = createRoot(rootElement);
+root.render(<div>This is C</div>);
